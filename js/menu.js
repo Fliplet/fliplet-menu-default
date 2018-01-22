1674cb121074a08adbfa38df96867ea94c9d1e23
hcarneiro <hcarneiro@weboo.biz>
2018-01-22 16:03:49 +0000

    Hide on scroll & swipe

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,9 @@
+var $menuElement = $('[data-name="Dropdown"]');
+var menuInstanceId = $menuElement.data('id');
+var data = Fliplet.Widget.getData(menuInstanceId) || {};
+
+var lastScrollTop = 0;
+
 if (Modernizr.backdropfilter) {
   $('.body').addClass('backdropfilter');
 }
@@ -11,3 +17,21 @@ $('[open-about-overlay]').on('click', function() {
     action: 'about-overlay'
   });
 });
+
+$('.fl-menu-swipe-handler').hammer().bind('swiperight', function() {
+  Fliplet.Navigate.back();
+});
+
+if (data.hide) {
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop){
+      // downscroll code
+      $('body').addClass('fl-menu-hidden');
+    } else {
+      // upscroll code
+      $('body').removeClass('fl-menu-hidden');
+    }
+    lastScrollTop = st;
+  });
+}
